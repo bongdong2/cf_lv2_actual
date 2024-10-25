2f6d457809c79e334433d0175855904131cf706c
좌승의 <seungui821@ohmycompany.com>
2024-10-25 15:59:46 +0900

    섹션 18 장바구니 스크린 완료 주문 완료까지함

--- lib/user/provider/auth_provider.dart
+++ lib/user/provider/auth_provider.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:actual/common/view/root_tab.dart';
 import 'package:actual/common/view/splash_screen.dart';
+import 'package:actual/order/view/order_done_screen.dart';
 import 'package:actual/restaurant/view/basket_screen.dart';
 import 'package:actual/restaurant/view/restaurant_detail_screen.dart';
 import 'package:actual/user/provider/user_me_proivder.dart';
@@ -53,6 +54,11 @@ class AuthProvider extends ChangeNotifier {
           path: '/basket',
           name: BasketScreen.routeName,
           builder: (_, state) => BasketScreen(),
+        ),
+        GoRoute(
+          path: '/order_done',
+          name: OrderDoneScreen.routeName,
+          builder: (_, state) => OrderDoneScreen(),
         ),
         GoRoute(
           path: '/splash',
