87d92b3789957a9476887eedebe662b8d5b9dfdc
좌승의 <seungui821@ohmycompany.com>
2024-10-26 13:34:37 +0900

    debounce

--- lib/user/provider/basket_provider.dart
+++ lib/user/provider/basket_provider.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:actual/user/model/basket_item_model.dart';
+import 'package:debounce_throttle/debounce_throttle.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../product/model/product_model.dart';
@@ -19,10 +20,21 @@ final basketProvider =
 
 class BasketProvider extends StateNotifier<List<BasketItemModel>> {
   final UserMeRepository repository;
+  final updateBasketDebounce = Debouncer(
+    Duration(seconds: 1),
+    initialValue: null,
+    checkEquality: false,
+  );
 
   BasketProvider({
     required this.repository,
-  }) : super([]);
+  }) : super([]){
+    updateBasketDebounce.values.listen(
+        (event) {
+          patchBasket();
+        },
+    );
+  }
 
   Future<void> patchBasket() async {
     await repository.patchBasket(
@@ -70,7 +82,7 @@ class BasketProvider extends StateNotifier<List<BasketItemModel>> {
 
     // Optimistic Response(긍정적 응답)
     // 응답이 성공할거라고 가정하고 상태를 먼저 업데이트함
-    await patchBasket();
+    updateBasketDebounce.setValue(null);
   }
 
   Future<void> removeFromBasket({
@@ -110,6 +122,6 @@ class BasketProvider extends StateNotifier<List<BasketItemModel>> {
           .toList();
     }
 
-    await patchBasket();
+    updateBasketDebounce.setValue(null);
   }
 }
